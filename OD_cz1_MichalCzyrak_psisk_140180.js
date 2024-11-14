3a85a637a5aee1b8ce568c79d73e24fcb904dadd
MC140180 <162018489+MC140180@users.noreply.github.com>
2024-11-14 21:42:34 +0100

    Add files via upload

--- OD_cz1_MichalCzyrak_psisk_140180.js
+++ OD_cz1_MichalCzyrak_psisk_140180.js
@@ -1,10 +1,9 @@
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 function encodeCeasar(word, shiftNumber) {
-    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  
     return word.toUpperCase().split('').map((element) => {
-        const index = alphabet.indexOf(element);
-        if (index !== -1) {
-            return alphabet[(index + shiftNumber) % 26];
+        if (alphabet.indexOf(element) !== -1) {
+            return alphabet[(alphabet.indexOf(element) + shiftNumber) % 26];
         }
         return element; 
         
@@ -12,11 +11,8 @@ function encodeCeasar(word, shiftNumber) {
 }
 
 function decodeCeasar(word, shiftNumber){
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
     return word.toUpperCase().split('').map((element) => {
-      const ix = alphabet.indexOf(element);
-        if(ix === -1) {
+        if(alphabet.indexOf(element)=== -1) {
           return element
         }
       return alphabet.split('').at([alphabet.indexOf(element)-shiftNumber])
@@ -24,13 +20,12 @@ function decodeCeasar(word, shiftNumber){
 }
 
 function encodeVigenere(word, key){
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     key = key.toUpperCase()
 
     return word.toUpperCase().split('').map((element, index) => {
           const shift = (alphabet.indexOf(element) + alphabet.indexOf(key[index % key.length])) % 26;
-          const ix = alphabet.indexOf(element);
-            if(ix === -1) {
+          
+            if(alphabet.indexOf(element)=== -1) {
               return element
             }
           return alphabet[shift];
@@ -38,12 +33,10 @@ function encodeVigenere(word, key){
 }
 
 function decodeVigenere(word, key){
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     key = key.toUpperCase()
 
     return word.toUpperCase().split('').map((element, index) => {
-      const ix = alphabet.indexOf(element);
-        if(ix === -1) {
+        if(alphabet.indexOf(element) === -1) {
           return element
         }
       return alphabet.split('').at((alphabet.indexOf(element) - alphabet.indexOf(key[index % key.length])))
@@ -52,25 +45,21 @@ function decodeVigenere(word, key){
 }
 
 function breakTheCode(word, recordsNumber) {
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   word = word.toUpperCase();
   let decryptedWords = [''];
   for (let i = 0; i < 26; i++) {
       const decryptedWord = word.split('').map(sign => {
-      const ix = alphabet.indexOf(sign);
-        if(ix === -1) {
+        if(alphabet.indexOf(sign)=== -1) {
           return sign
         }
-
-          const newIndex = (alphabet.indexOf(sign) - i + 26) % 26; 
-          return alphabet[newIndex];
+          return alphabet[(alphabet.indexOf(sign) - i + 26) % 26];
       });
 
       const aCount = decryptedWord.filter(letter => letter === 'A').length;
-
       if (aCount >=  1) {
           decryptedWords.push(decryptedWord.join(''));
       }
+
       decryptedWords.sort((a, b) => {
         const countA = a.split('').filter(letter => letter === 'A').length;
         const countB = b.split('').filter(letter => letter === 'A').length;
